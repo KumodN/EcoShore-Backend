6fd970f7a8b9d38a54adbd276cefbd73bcf1a38f
Chamika Thennakoon <thennakoonchamika11@gmail.com>
2026-04-11 23:13:43 +0530

    feat: fixing firebase config

--- src/providers/FirebaseChatProvider.js
+++ src/providers/FirebaseChatProvider.js
@@ -1,6 +1,6 @@
 const IChatProvider = require('../interfaces/IChatProvider');
 const logger = require('../config/logger');
-const FIREBASE_CONFIG = require('../config/firebase-service-account.json');
+const FIREBASE_CONFIG = require('../config/firebase');
 
 /**
  * FirebaseChatProvider (SOLID - Single Responsibility)
@@ -9,7 +9,7 @@ const FIREBASE_CONFIG = require('../config/firebase-service-account.json');
  *
  * Requirements:
  * - npm install firebase-admin
- * - Firebase service account key at: config/firebase-service-account.json
+ * - Firebase service account fields via env variables (or JSON env blob)
  * - FIREBASE_DATABASE_URL in .env
  */
 
@@ -28,6 +28,28 @@ class FirebaseChatProvider extends IChatProvider {
 
     try {
       const admin = require('firebase-admin');
+      const requiredFields = ['project_id', 'client_email', 'private_key'];
+      const missingFields = requiredFields.filter(
+        (field) => !FIREBASE_CONFIG[field]
+      );
+
+      if (missingFields.length > 0) {
+        throw new Error(
+          `Missing Firebase credentials fields: ${missingFields.join(', ')}. Configure FIREBASE_* env variables.`
+        );
+      }
+
+      if (!FIREBASE_CONFIG.private_key.includes('BEGIN PRIVATE KEY')) {
+        throw new Error(
+          'Invalid Firebase private key format. Ensure FIREBASE_PRIVATE_KEY preserves line breaks using \\n in .env.'
+        );
+      }
+
+      if (!process.env.FIREBASE_DATABASE_URL) {
+        throw new Error(
+          'Missing FIREBASE_DATABASE_URL environment variable.'
+        );
+      }
 
       if (!admin.apps.length) {
         admin.initializeApp({
